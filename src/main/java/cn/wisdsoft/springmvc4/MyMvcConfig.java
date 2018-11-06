f3d4baefba0fdfc6edb6c84466c3048119b0981e
sunweiguang <953124935@qq.com>
2018-11-06 12:31:04 +0800

    Spring MVC静态资源映射

--- src/main/java/cn/wisdsoft/springmvc4/MyMvcConfig.java
+++ src/main/java/cn/wisdsoft/springmvc4/MyMvcConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -18,7 +20,7 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan("cn.wisdsoft.springmvc4")
-public class MyMvcConfig {
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -27,5 +29,13 @@ public class MyMvcConfig {
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
         return viewResolver;
+    }
+
+    //静态资源映射
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //addResourceLocations指的是文件配置的目录，addResourceHandler指的是对外暴露的访问路径
+        registry.addResourceHandler("/assets/**").
+                addResourceLocations("classpath:/assets/");
     }
 }
